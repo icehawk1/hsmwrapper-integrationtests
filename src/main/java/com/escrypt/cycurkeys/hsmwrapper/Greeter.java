c5b2e28e3cc01d21701df6a7a2bd8e74d19d297b
Martin Haug <martinhaug@smart-mail.de>
2017-12-01 17:49:53 +0100

    + Kann Test ausführen

---
+++ src/main/java/com/escrypt/cycurkeys/hsmwrapper/Greeter.java
@@ -0,0 +1,15 @@
+package com.escrypt.cycurkeys.hsmwrapper;
+import java.io.PrintStream;
+
+/**
+ * A component for creating personal greetings.
+ */
+public class Greeter {
+    public void greet(PrintStream to, String name) {
+        to.println(createGreeting(name));
+    }
+
+    public String createGreeting(String name) {
+        return "Hello, " + name + "!";
+    }
+}
